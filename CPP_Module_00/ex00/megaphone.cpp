c1567e7846e1e52b7a069d60943d6179990264ef
Lukasz Tomaszewski <ltomasze@c3r7s3.42warsaw.pl>
2025-04-27 16:04:42 +0200

    change ex00 for c++

--- CPP_Module_00/ex00/megaphone.cpp
+++ CPP_Module_00/ex00/megaphone.cpp
@@ -6,13 +6,36 @@
 /*   By: ltomasze <ltomasze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 13:25:35 by ltomasze          #+#    #+#             */
-/*   Updated: 2025/04/24 14:05:40 by ltomasze         ###   ########.fr       */
+/*   Updated: 2025/04/27 16:02:14 by ltomasze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cctype>
 
-int main (int ac, char **av)
+int main(int ac, char **av)
+{
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	}
+	else
+	{
+		for (int i = 1; i < ac; i++)
+		{
+			for (int j = 0; av[i][j]; j++)
+			{
+				std::cout << (char)toupper(av[i][j]);
+			}
+			if (i < ac - 1)
+				std::cout << " ";
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
+
+/*int main (int ac, char **av)
 {
 	int i = 1;
 	int j = 0;
@@ -42,4 +65,4 @@ int main (int ac, char **av)
 	}
 	std::cout << "\n";
 	return (0);
-}
+}*/
