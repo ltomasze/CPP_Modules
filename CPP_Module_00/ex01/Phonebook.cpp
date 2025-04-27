a08e6c15dc4ac17d1c4bf295758e1189c079d4dd
Lukasz Tomaszewski <ltomasze@c3r7s3.42warsaw.pl>
2025-04-27 13:59:00 +0200

    add to phone book all place no empty

--- CPP_Module_00/ex01/Phonebook.cpp
+++ CPP_Module_00/ex01/Phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: ltomasze <ltomasze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 17:09:20 by ltomasze          #+#    #+#             */
-/*   Updated: 2025/04/27 13:21:21 by ltomasze         ###   ########.fr       */
+/*   Updated: 2025/04/27 13:56:34 by ltomasze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,26 +28,46 @@ void PhoneBook::addContact()
 	Contact newContact;
 	std::string input;
 
-	std::cout << "Enter first name: ";
-	std::getline(std::cin, input);
-	newContact.setFirstName(input);
+	do {
+        std::cout << "Enter first name: ";
+        std::getline(std::cin, input);
+        if (input.empty())
+            std::cout << "First name cannot be empty. Please try again." << std::endl;
+    } while (input.empty());
+    newContact.setFirstName(input);
 
-	std::cout << "Enter last name: ";
-	std::getline(std::cin, input);
-	newContact.setLastName(input);
+	do {
+        std::cout << "Enter last name: ";
+        std::getline(std::cin, input);
+        if (input.empty())
+            std::cout << "Last name cannot be empty. Please try again." << std::endl;
+    } while (input.empty());
+    newContact.setLastName(input);
 
-	std::cout << "Enter nickname: ";
-	std::getline(std::cin, input);
-	newContact.setNickname(input);
+	do {
+        std::cout << "Enter nickname: ";
+        std::getline(std::cin, input);
+        if (input.empty())
+            std::cout << "Nickname cannot be empty. Please try again." << std::endl;
+    } while (input.empty());
+    newContact.setNickname(input);
 
-	std::cout << "Enter phone number: ";
-	std::getline(std::cin, input);
+	do {
+		std::cout << "Enter phone number: ";
+		std::getline(std::cin, input);
+		if (input.empty())
+			std::cout << "Phone number cannot be empty. Please try again." << std::endl;
+	} while (input.empty());
 	newContact.setPhoneNumber(input);
 
-	std::cout << "Enter darkest secret: ";
-	std::getline(std::cin, input);
-	newContact.setDarkestSecret(input);
-
+	do {
+        std::cout << "Enter darkest secret: ";
+        std::getline(std::cin, input);
+        if (input.empty())
+            std::cout << "Darkest secret cannot be empty. Please try again." << std::endl;
+    } while (input.empty());
+    newContact.setDarkestSecret(input);
+	
 	contacts[nextIndex] = newContact;
 	
 	nextIndex = (nextIndex + 1) % 8;
