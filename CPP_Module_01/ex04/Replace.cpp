7eb296bb6a25bae210a7fc5ec310903664fd0aa2
Lukasz Tomaszewski <lukasz.tomaszewski2023@gmail.com>
2025-05-17 16:41:35 +0200

    add ex04 in progress

---
+++ CPP_Module_01/ex04/Replace.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Replace.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ltomasze <ltomasze@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/17 15:21:24 by ltomasze          #+#    #+#             */
+/*   Updated: 2025/05/17 15:51:43 by ltomasze         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Replace.hpp"
+
+Replace::Replace(const std::string& filename, const std::string& s1, const std:: string& s2)
+	: _filename(filename), _s1(s1), _s2(s2) {}
+
+Replace::~Replace() {}
+
+bool Replace::processFile() const
+{
+	
+}
---
+++ CPP_Module_01/ex04/Replace.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Replace.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ltomasze <ltomasze@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/17 15:21:37 by ltomasze          #+#    #+#             */
+/*   Updated: 2025/05/17 15:30:32 by ltomasze         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef REPLACE_HPP
+# define REPLACE_HPP
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class Replace
+{
+	private:
+		std::string _filename;
+		std::string _s1;
+		std::string _s2;
+	public:
+		Replace(const std::string& filename, const std::string& s1, const std::string& s2);
+		~Replace();
+
+		bool processFile() const;
+};
+
+#endif
