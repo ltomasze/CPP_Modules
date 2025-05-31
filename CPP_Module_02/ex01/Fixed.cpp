a64bf2dfc0fdfed5cf21a33c5932d86170fe7a40
Lukasz Tomaszewski <lukasz.tomaszewski2023@gmail.com>
2025-05-31 18:52:01 +0200

    ex01 ready to eval

--- CPP_Module_02/ex01/Fixed.cpp
+++ CPP_Module_02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: ltomasze <ltomasze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 15:16:01 by ltomasze          #+#    #+#             */
-/*   Updated: 2025/05/31 17:22:15 by ltomasze         ###   ########.fr       */
+/*   Updated: 2025/05/31 18:44:02 by ltomasze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,14 @@ Fixed::Fixed(int value)
 {
 	std::cout << "Int constructor called" << std::endl;
 	this->_fixedPointValue = value << _fractionalBits;
+	//10 << 8 = 10 * 256 = 2560
 }
 
 Fixed::Fixed(float value)
 {
 	std::cout << "Float constructor called" << std::endl;
 	this->_fixedPointValue = roundf(value * (1 << _fractionalBits));
+	/* 1 << 256 next 42.42 * 256 = 10859.52 czyli najbliżej do 10860*/
 }
 
 Fixed::~Fixed()
@@ -66,11 +68,15 @@ void Fixed::setRawBits(int const raw)
 float Fixed::toFloat(void) const
 {
 	return (float)this->_fixedPointValue / (1 << _fractionalBits);
+	/*ponieważ _fixedPointValue jest stałoprzecinkowa to należy dokonać 
+	rzutowania bo float jest zmiennoprzecinkową*/
 }
 
 int Fixed::toInt(void) const
 {
 	return this->_fixedPointValue >> _fractionalBits;
+	/* lub liczba dzielona przez 256 bo 8 bitów
+	stałoprzecinkowa na całkowitą*/
 }
 
 std::ostream &operator<<(std::ostream &out, const Fixed &fixed)
@@ -78,3 +84,15 @@ std::ostream &operator<<(std::ostream &out, const Fixed &fixed)
 	out << fixed.toFloat();
 	return out;
 }
+/*przeciązenie operatora << służy do wypyisywania różnego rodzaju typu danych
+na wskazany typ, jest poza klasą bo jest globalny bo ma std::ostream
+
+ostream to strumień wyjściowy
+
+std::ostream &operator<<(std::ostream &out, const YourClass &obj)
+{
+    Wypisz dane obiektu obj do strumienia out
+    out << obj.someData; Możesz użyć metod lub pól obiektu obj
+    Zwróć referencję do strumienia wyjściowego
+    return out;
+}*/
