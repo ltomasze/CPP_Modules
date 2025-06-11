8db00cbf913c1b529293bbbec651dc0bbe724559
Lukasz Tomaszewski <lukasz.tomaszewski2023@gmail.com>
2025-06-11 10:59:50 +0200

    add CPP03, ex00

---
+++ CPP_Module_03/ex00/main.cpp
@@ -0,0 +1,31 @@
+#include "ClapTrap.hpp"
+
+int main()
+{
+    ClapTrap noName;
+    ClapTrap bot1("Bot1");
+    ClapTrap bot2("Bot2");
+    noName = bot1;
+
+    for (int i = 0; i < 11; i++)
+    {
+        bot1.attack("Bot2");
+        bot2.takeDamage(1);
+        bot2.beRepaired(1);
+    }
+    return 0;
+}
+/*
+int main()
+{
+    ClapTrap bot1("Bot1");
+    ClapTrap bot2("Bot2");
+
+    for (int i = 0; i < 3; i++)
+    {
+        bot1.attack("Bot2");
+        bot2.takeDamage(5);
+        bot2.beRepaired(1);
+    }
+    return 0;
+}*/
