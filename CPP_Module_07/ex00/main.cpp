de79373d226f2b8a2a930596164e196ebc6b1945
Lukasz Tomaszewski <lukasz.tomaszewski2023@gmail.com>
2025-07-06 10:41:49 +0200

    CPP07 ex00 add Makefile, main.cpp and whatever.hpp.cpp in raw status

---
+++ CPP_Module_07/ex00/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ltomasze <ltomasze@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/06 10:39:22 by ltomasze          #+#    #+#             */
+/*   Updated: 2025/07/06 10:39:45 by ltomasze         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int main( void ) 
+{
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
