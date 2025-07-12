73edd5ca8997bf20c401d4d9a30339493e3b5212
Lukasz Tomaszewski <lukasz.tomaszewski2023@gmail.com>
2025-07-12 17:45:30 +0200

    ex01 ready to eval

--- CPP_Module_08/ex01/Span.hpp
+++ CPP_Module_08/ex01/Span.hpp
@@ -6,8 +6,42 @@
 /*   By: ltomasze <ltomasze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 14:25:56 by ltomasze          #+#    #+#             */
-/*   Updated: 2025/07/12 14:26:03 by ltomasze         ###   ########.fr       */
+/*   Updated: 2025/07/12 17:17:07 by ltomasze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+#include <vector> // for container, size
+#include <exception>
+#include <algorithm> //for sort, min_element, max_element
+#include <iostream>
+#include <climits>
+
+class Span
+{
+	private:
+		unsigned int _maxSize;
+		std::vector<int> _numbers;
+		Span();
+	public:
+		Span(unsigned int_maxSize);
+		Span(const Span& other);
+		Span& operator=(const Span& other);
+		~Span();
+
+		void addNumber(int number);
+		void addNumbers(std::vector<int>::iterator begin, std::vector<int>::iterator end);
+		int shortestSpan() const;
+		int longestSpan() const;
+		
+		class SpanFullException: public std::exception
+		{
+			const char* what() const throw(); 
+			//no need add virtual for begin becausse "what" in basic is always virtual
+		};
+		class NotEnoughNumbersException : public std::exception 
+		{
+        	const char* what() const throw();
+    	};
+
+};
--- CPP_Module_08/ex01/main.cpp
+++ CPP_Module_08/ex01/main.cpp
@@ -6,12 +6,11 @@
 /*   By: ltomasze <ltomasze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 14:24:49 by ltomasze          #+#    #+#             */
-/*   Updated: 2025/07/12 14:25:35 by ltomasze         ###   ########.fr       */
+/*   Updated: 2025/07/12 17:40:23 by ltomasze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
-#include <iostream>
 
 int main()
 {
@@ -25,3 +24,28 @@ std::cout << sp.shortestSpan() << std::endl;
 std::cout << sp.longestSpan() << std::endl;
 return 0;
 }
+
+// int main()
+// {
+//     try 
+// 	{
+//         Span sp(10000);
+//         for (int i = 0; i < 5000; ++i)
+// 		{
+//             sp.addNumber(i);
+//         }
+//         std::vector<int> sp1;
+//         for (int i = 15000; i < 20000; ++i) 
+// 		{
+//             sp1.push_back(i);
+//         }
+//         sp.addNumbers(sp1.begin(), sp1.end());
+//         std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
+//         std::cout << "Longest span: " << sp.longestSpan() << std::endl;
+//     } 
+// 	catch (const std::exception& e) 
+// 	{
+//         std::cerr << e.what() << std::endl;
+//     }
+//     return 0;
+// }
