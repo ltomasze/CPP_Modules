30c69808d21ba89b97ecab1c772bedbf9c207973
Lukasz Tomaszewski <lukasz.tomaszewski2023@gmail.com>
2025-09-14 14:57:31 +0200

    add input.txt and work in main

--- CPP_Module_09/ex00/main.cpp
+++ CPP_Module_09/ex00/main.cpp
@@ -6,7 +6,43 @@
 /*   By: ltomasze <ltomasze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 14:12:46 by ltomasze          #+#    #+#             */
-/*   Updated: 2025/09/09 14:12:47 by ltomasze         ###   ########.fr       */
+/*   Updated: 2025/09/14 14:26:39 by ltomasze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char** argv) 
+{
+    if (argc != 2)
+	{
+        std::cerr << "Error: could not open file." << std::endl;
+        return (1);
+    }
+    std::ifstream file(argv[1]);
+    if (!file.is_open()) 
+	{
+        std::cerr << "Error: could not open file." << std::endl;
+        return (1);
+    }
+	std::string line;
+	while (std::getline(file, line))
+	{
+	    if (line.empty())
+	        continue;
+	    std::string::size_type pipePos = line.find('|');
+	    if (pipePos == std::string::npos)//npos special value for find when dont find:)
+		{
+	        std::cerr << "Error: bad input => " << line << std::endl;
+	        continue;
+	    }
+	    std::string date = line.substr(0, pipePos);
+	    std::string value = line.substr(pipePos + 1);
+	    // Tymczasowo wypisz poprawne linie
+	    std::cout << date << "|"<< value << std::endl;
+	}
+    return 0;
+}
 
