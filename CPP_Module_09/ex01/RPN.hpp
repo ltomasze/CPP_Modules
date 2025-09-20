f89bb57396187176913b86abdfebaeb3bebdfa95
Lukasz Tomaszewski <lukasz.tomaszewski2023@gmail.com>
2025-09-20 18:27:45 +0200

    delete header stdexcept because I dont use, exercise is finish

--- CPP_Module_09/ex01/RPN.hpp
+++ CPP_Module_09/ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: ltomasze <ltomasze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/20 16:31:58 by ltomasze          #+#    #+#             */
-/*   Updated: 2025/09/20 17:06:43 by ltomasze         ###   ########.fr       */
+/*   Updated: 2025/09/20 18:26:20 by ltomasze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,8 @@
 
 #include <string>
 #include <stack>
-#include <stdexcept>
 #include <iostream>
-#include <cctype>
+#include <cctype> //for isspace
 
 class RPN
 {
