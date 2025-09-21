351000d2724fa65a8c1e694a8b057b3551416753
Lukasz Tomaszewski <lukasz.tomaszewski2023@gmail.com>
2025-09-21 10:33:53 +0200

    first step in ex02, i have main and .hpp

--- CPP_Module_09/ex02/PmergeMe.hpp
+++ CPP_Module_09/ex02/PmergeMe.hpp
@@ -6,7 +6,33 @@
 /*   By: ltomasze <ltomasze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/20 18:41:04 by ltomasze          #+#    #+#             */
-/*   Updated: 2025/09/20 18:41:05 by ltomasze         ###   ########.fr       */
+/*   Updated: 2025/09/21 10:30:42 by ltomasze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <vector>
+#include <deque>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <ctime>
+
+class PmergeMe
+{
+	public:
+		PmergeMe();
+    	PmergeMe(const PmergeMe& other);
+    	PmergeMe& operator=(const PmergeMe& other);
+    	~PmergeMe(); 
+
+		void processInput(const std::vector<int>& input);
+		void displayResults() const;
+
+	private:
+    	std::vector<int> containerA;
+    	std::deque<int> containerB;
+    	double timeA;
+    	double timeB;
+};
--- CPP_Module_09/ex02/main.cpp
+++ CPP_Module_09/ex02/main.cpp
@@ -6,7 +6,39 @@
 /*   By: ltomasze <ltomasze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/20 18:40:30 by ltomasze          #+#    #+#             */
-/*   Updated: 2025/09/20 18:40:31 by ltomasze         ###   ########.fr       */
+/*   Updated: 2025/09/21 10:24:27 by ltomasze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PmergeMe.hpp"
+
+int main(int argc, char* argv[]) 
+{
+    if (argc < 2) 
+	{
+        std::cerr << "Error\n";
+        return 1;
+    }
+    std::vector<int> input;
+    for (int i = 1; i < argc; ++i) 
+	{
+        std::istringstream iss(argv[i]);
+        int num;
+        if (!(iss >> num) || num <= 0) { // check argv, iis >> num <- this is convert str for int
+            std::cerr << "Error\n";
+            return 1;
+        }
+        input.push_back(num); //add num for end od containers
+    }
+    std::cout << "Before: ";
+    for (size_t i = 0; i < input.size(); ++i) 
+	{
+        std::cout << input[i] << " ";
+    }
+    std::cout << std::endl;
+    // Utworzenie obiektu klasy PmergeMe i przetworzenie danych
+    PmergeMe sorter;
+    sorter.processInput(input);
+    sorter.displayResults();
+    return 0;
+}
